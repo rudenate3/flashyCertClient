1f8b57b52ab7ab7ce6080bd82c64e2859af08cdf
rudenate3 <nathan.odin@gmail.com>
2018-07-29 22:38:15 -0400

    Fix overlooked updates
    -(refactor) Didn't update all of copied component

--- src/js/components/registration.component.js
+++ src/js/components/registration.component.js
@@ -1,12 +1,12 @@
 export const RegistrationComponent = `
-  <h1>Login</h1>
-  <form id="login-form">
+  <h1>Registration</h1>
+  <form id="register-form">
     <label for="username">Username</label>
     <input type="text" id="username">
     <label for="email">Email</label>
     <input type="text" id="email">
     <label for="password">Password</label>
     <input type="password" id="password">
-    <button>Login</button>
+    <button>Register</button>
   </form>
 `
