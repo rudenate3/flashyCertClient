7615e3a806ca6d46f2eca4e425ed621bf5284d92
Nathan Dunn <nathan.odin@gmail.com>
2018-07-30 11:54:19 -0400

    Expand Login and Registration components
    -(refactor) Expand Login and Registration components like HomeComponent

--- src/js/components/login.component.js
+++ src/js/components/login.component.js
@@ -1,10 +1,30 @@
-export const LoginComponent = `
-  <h1>Login</h1>
-  <form id="login-form">
-    <label for="email">Email</label>
-    <input type="text" id="email">
-    <label for="password">Password</label>
-    <input type="password" id="password">
-    <button>Login</button>
-  </form>
-`
+import { State } from '../state'
+
+export class LoginComponent {
+  constructor(container) {
+    this.container = container
+  }
+
+  render() {
+    this.container.innerHTML = this.markup()
+    this.addEventListeners()
+  }
+
+  markup() {
+    return `
+      <h1>Login</h1>
+      <form id="login-form">
+        <label for="email">Email</label>
+        <input type="text" id="email">
+        <label for="password">Password</label>
+        <input type="password" id="password">
+        <button>Login</button>
+      </form>
+      <button id='back'>Back</button>
+      `
+  }
+
+  addEventListeners(){
+    document.querySelector('#back').addEventListener('click', () => State.router.changeRoute('home'))
+  }
+}
--- src/js/components/registration.component.js
+++ src/js/components/registration.component.js
@@ -1,12 +1,32 @@
-export const RegistrationComponent = `
-  <h1>Registration</h1>
-  <form id="register-form">
-    <label for="username">Username</label>
-    <input type="text" id="username">
-    <label for="email">Email</label>
-    <input type="text" id="email">
-    <label for="password">Password</label>
-    <input type="password" id="password">
-    <button>Register</button>
-  </form>
-`
+import { State } from '../state'
+
+export class RegistrationComponent {
+  constructor(container) {
+    this.container = container
+  }
+
+  render() {
+    this.container.innerHTML = this.markup()
+    this.addEventListeners()
+  }
+
+  markup() {
+    return `
+      <h1>Registration</h1>
+      <form id="register-form">
+        <label for="username">Username</label>
+        <input type="text" id="username">
+        <label for="email">Email</label>
+        <input type="text" id="email">
+        <label for="password">Password</label>
+        <input type="password" id="password">
+        <button>Register</button>
+      </form>
+      <button id='back'>Back</button>
+      `
+  }
+
+  addEventListeners(){
+    document.querySelector('#back').addEventListener('click', () => State.router.changeRoute('home'))
+  }
+}
