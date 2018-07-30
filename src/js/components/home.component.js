56a4791adef5d7f84003cebf71826c2a138000f6
Nathan Dunn <nathan.odin@gmail.com>
2018-07-30 11:53:09 -0400

    Expand Home component
    -(refactor) Expand Home component to do more than just return template
                literal.  Use to test future components.

--- src/js/components/home.component.js
+++ src/js/components/home.component.js
@@ -1,5 +1,25 @@
-export const HomeComponent = `
-  <h1>FlashyCerts</h1>
-  <button>Login</button>
-  <button>Register</button>
-`
+import { State } from '../state'
+
+export class HomeComponent {
+  constructor(container) {
+    this.container = container
+  }
+
+  render() {
+    this.container.innerHTML = this.markup()
+    this.addEventListeners()
+  }
+
+  markup() {
+  return `
+    <h1>FlashyCerts</h1>
+    <button id='go-to-login-btn'>Login</button>
+    <button id='go-to-registration-btn'>Register</button>
+    `
+  }
+
+  addEventListeners(){
+    document.querySelector('#go-to-login-btn').addEventListener('click', () => State.router.changeRoute('login'))
+    document.querySelector('#go-to-registration-btn').addEventListener('click', () => State.router.changeRoute('registration'))
+  }
+}
