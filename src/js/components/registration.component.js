1857a2b3911bf2f80b44f38d2553871b160de132
rudenate3 <nathan.odin@gmail.com>
2018-07-29 22:36:07 -0400

    Add Registration Component
    -(feature) Add basic unstyled Registration Component

---
+++ src/js/components/registration.component.js
@@ -0,0 +1,12 @@
+export const RegistrationComponent = `
+  <h1>Login</h1>
+  <form id="login-form">
+    <label for="username">Username</label>
+    <input type="text" id="username">
+    <label for="email">Email</label>
+    <input type="text" id="email">
+    <label for="password">Password</label>
+    <input type="password" id="password">
+    <button>Login</button>
+  </form>
+`
