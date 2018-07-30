980abf924f05a10986a67a7171c862161084e9da
rudenate3 <nathan.odin@gmail.com>
2018-07-29 23:57:43 -0400

    Add Login and Registration Components to routes
    -(refactor) Add Login and Registration Components to routes

--- src/js/routing/routes.js
+++ src/js/routing/routes.js
@@ -1,7 +1,9 @@
 import { HomeComponent } from '../components/home.component'
+import { LoginComponent } from '../components/login.component'
+import { RegistrationComponent } from '../components/registration.component'
 
 export const Routes = {
   home: HomeComponent,
-  login: 'LoginComponent',
-  registration: 'RegistrationComponent'
+  login: LoginComponent,
+  registration: RegistrationComponent
 }
