874328b4ae1eb47d0c3e5458f22f0990bda2c36c
rudenate3 <nathan.odin@gmail.com>
2018-07-29 22:31:58 -0400

    Add Login Component
    -(feature) Add basic unstyled Login Component

---
+++ src/js/components/login.component.js
@@ -0,0 +1,10 @@
+export const LoginComponent = `
+  <h1>Login</h1>
+  <form id="login-form">
+    <label for="email">Email</label>
+    <input type="text" id="email">
+    <label for="password">Password</label>
+    <input type="password" id="password">
+    <button>Login</button>
+  </form>
+`
