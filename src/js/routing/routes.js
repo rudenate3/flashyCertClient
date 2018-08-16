e49b80d3f5d72b8692443afb132f74c605893783
rudenate3 <nathan.odin@gmail.com>
2018-08-16 15:38:37 -0400

    Add new Exam and Question crud routes
    -(feature) Add new Exam and Question crud routes

--- src/js/routing/routes.js
+++ src/js/routing/routes.js
@@ -2,10 +2,32 @@ import { HomeComponent } from '../components/home.component'
 import { LoginComponent } from '../components/login.component'
 import { RegistrationComponent } from '../components/registration.component'
 import { DashboardComponent } from '../components/dashboard.component'
+import { ExamCreateComponent } from '../components/exams/examCreate.component'
+import { ExamDestroyComponent } from '../components/exams/examDestroy.component'
+import { ExamIndexComponent } from '../components/exams/examIndex.component'
+import { ExamShowComponent } from '../components/exams/examShow.component'
+import { ExamUpdateComponent } from '../components/exams/examUpdate.component'
+import { QuestionCreateComponent } from '../components/questions/questionCreate.component'
+import { QuestionDestroyComponent } from '../components/questions/questionDestroy.component'
+import { QuestionIndexComponent } from '../components/questions/questionIndex.component'
+import { QuestionShowComponent } from '../components/questions/questionShow.component'
+import { QuestionUpdateComponent } from '../components/questions/questionUpdate.component'
 
 export const Routes = {
   home: HomeComponent,
   login: LoginComponent,
   registration: RegistrationComponent,
-  dashboard: DashboardComponent
+  dashboard: DashboardComponent,
+  // Exam Routes
+  examCreate: ExamCreateComponent,
+  examDestroy: ExamDestroyComponent,
+  examIndex: ExamIndexComponent,
+  examShow: ExamShowComponent,
+  examUpdate: ExamUpdateComponent,
+  // Question Routes
+  questionCreate: QuestionCreateComponent,
+  questionDestroy: QuestionDestroyComponent,
+  questionIndex: QuestionIndexComponent,
+  questionShow: QuestionShowComponent,
+  questionUpdate: QuestionUpdateComponent
 }
