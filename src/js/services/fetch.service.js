3c2660636cde27715a3972616a3b341a3f6d085c
Nathan Dunn <nathan.odin@gmail.com>
2018-08-16 19:07:14 -0400

    Add other 3 methods to Fetch Service
    -(feature) Add other 3 methods to Fetch Service

--- src/js/services/fetch.service.js
+++ src/js/services/fetch.service.js
@@ -2,7 +2,18 @@ import { Config } from '../config/config'
 
 const api = Config.api
 
-export const Get = route => {}
+export const Get = async (route, token) => {
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    }
+  }
+  if(token) requestOptions.headers.Authorization = token
+
+  return await fetch(`${api}${route}`, requestOptions)
+}
+
 export const Post = async (route, data, token) => {
   
   const requestOptions = {
@@ -17,7 +28,31 @@ export const Post = async (route, data, token) => {
 
   return await fetch(`${api}${route}`, requestOptions)
 }
-export const Patch = route => {}
-export const Delete = route => {}
+
+export const Patch = async (route, data, token) => {
+
+  const requestOptions = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    }
+  }
+  if(token) requestOptions.headers.Authorization = token
+
+  return await fetch(`${api}${route}`, requestOptions)
+}
+
+export const Delete = async (route, token) => {
+
+  const requestOptions = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    }
+  }
+  if(token) requestOptions.headers.Authorization = token
+
+  return await fetch(`${api}${route}`, requestOptions)
+}
 
 
