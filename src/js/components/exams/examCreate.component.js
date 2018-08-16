0ae9850c6ca48975a6a580f44f2a61344327ec4a
rudenate3 <nathan.odin@gmail.com>
2018-08-16 17:14:52 -0400

    Configure Exam Create
    -(feature) Configure Exam Create Component

--- src/js/components/exams/examCreate.component.js
+++ src/js/components/exams/examCreate.component.js
@@ -5,17 +5,52 @@ export class ExamCreateComponent {
     this.container = container
   }
 
+  destroy() {
+    this.removeEventListeners()
+  }
+
   render() {
     if (!State.token) return State.router.changeRoute('login')
     this.container.innerHTML = this.markup()
     this.addEventListeners()
+    this.destroy = this.destroy
   }
 
   markup() {
     return `
     <h1>Exam Create</h1>
+    <span id="messages"></span>
+    <form id="exam-create-form">
+      <label for="title">Title</label>
+      <input type="text" id="title">
+      <label for="passing">Passing</label>
+      <input type="text" id="passing">
+      <button>Create Exam</button>
+    </form>
+    <button id='back'>Back</button>
     `
   }
 
-  addEventListeners() {}
+  onBack() {
+    State.router.goToDashboard()
+  }
+
+  onExamCreateClick(event) {
+    event.preventDefault()
+    console.log('submitted')
+  }
+
+  addEventListeners() {
+    document.querySelector('#back').addEventListener('click', this.onBack)
+    document
+      .querySelector('#exam-create-form')
+      .addEventListener('submit', this.onExamCreateClick)
+  }
+
+  removeEventListeners() {
+    document.querySelector('#back').removeEventListener('click', this.onBack)
+    document
+      .querySelector('#exam-create-form')
+      .removeEventListener('submit', this.onExamCreateClick)
+  }
 }
